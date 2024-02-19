0796a47599ea519d71ce4a92e415a4df9d9c6e60
carlosmaeda3d <carlosmaeda3d@yahoo.com>
2024-02-19 09:06:42 -0500

    commit2

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ rosterData = pd.read_csv('2023 Phillies Season Roster.csv')
 uniformData = pd.read_csv('2023 Phillies Uniform Numbers.csv')
 
 #Roster Data - Delete unwanted columns
+rosterData = rosterData.drop(['Unnamed: 2', 'Ht', 'Wt', 'DoB', 'WAR', 'Unnamed: 28'], axis=1)
 
 #Uniform Data - Swap column b with column a
 column_change = ['Name', '#']
@@ -18,8 +19,16 @@ newBatData = newBatData.drop(batData[batData['Pos'] == 'P'].index)
 #Add uniform numbers from uniformData to end of rosterData
 merge_rost_uni = rosterData.merge(uniformData, how='inner', on='Name')
 
-#Print the updated data
+#Rename new rosterData columns
+merge_rost_uni = merge_rost_uni.rename(columns={'#': 'Uniform#'})
+
+#Export data as a .csv in folder
+
+#   TEST AREA
 #print(uniformData)
 #print(rosterData)
-print(merge_rost_uni)
+#pd.options.display.max_columns = None
+print(merge_rost_uni.keys())
+test = merge_rost_uni[['Name', 'Age', 'B', 'T']]
+print(merge_rost_uni[:4])
 
