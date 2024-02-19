8a566b42e59df9839c5e6d0154dae7c8da684830
carlosmaeda3d <carlosmaeda3d@yahoo.com>
2024-02-19 00:09:52 -0500

    commit1

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import pandas as pd
 
 #Read .csv file
-batData = pd.read_csv('2023 Phillies Batting Data.csv', index_col= 0)
-rosterData = pd.read_csv('2023 Phillies Season Roster.csv', index_col=0)
-uniformData = pd.read_csv('2023 Phillies Uniform Numbers.csv', index_col=0)
+batData = pd.read_csv('2023 Phillies Batting Data.csv')
+rosterData = pd.read_csv('2023 Phillies Season Roster.csv')
+uniformData = pd.read_csv('2023 Phillies Uniform Numbers.csv')
 
 #Roster Data - Delete unwanted columns
+
+#Uniform Data - Swap column b with column a
+column_change = ['Name', '#']
+uniformData = uniformData.reindex(columns=column_change)
 
 #Batting Data - Remove last two rows and all pitcher rows seperately
 newBatData = batData.dropna(subset='Pos')
 newBatData = newBatData.drop(batData[batData['Pos'] == 'P'].index)
 
 #Add uniform numbers from uniformData to end of rosterData
+merge_rost_uni = rosterData.merge(uniformData, how='inner', on='Name')
 
 #Print the updated data
-print(rosterData)
+#print(uniformData)
+#print(rosterData)
+print(merge_rost_uni)
+
