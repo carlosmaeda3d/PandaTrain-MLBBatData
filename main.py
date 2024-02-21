37f95724db5944a1c152b1aaef5fa5982e6eaa53
carlosmaeda3d <carlosmaeda3d@yahoo.com>
2024-02-20 19:42:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,18 +6,24 @@ rosterData = pd.read_csv('2023 Phillies Season Roster.csv')
 uniformData = pd.read_csv('2023 Phillies Uniform Numbers.csv')
 
 #Roster Data - Delete unwanted columns
-rosterData = rosterData.drop(['Unnamed: 2', 'Ht', 'Wt', 'DoB', 'WAR', 'Unnamed: 28'], axis=1)
+newRosterData = rosterData.drop(['Unnamed: 2', 'Ht', 'Wt', 'DoB', 'WAR', 'Unnamed: 28'], axis=1)
 
 #Uniform Data - Swap column b with column a
 column_change = ['Name', '#']
 uniformData = uniformData.reindex(columns=column_change)
 
-#Batting Data - Remove last two rows and all pitcher rows seperately
+#Batting Data - Remove last two rows and all pitcher rows seperately, add 'b-' to all column name prefix, clean up
 newBatData = batData.dropna(subset='Pos')
 newBatData = newBatData.drop(batData[batData['Pos'] == 'P'].index)
 
+newBatData = newBatData.drop(['Rk', 'Age', 'OPS+', 'GDP', 'HBP', 'SH', 'SF', 'IBB', 'Name-additional'], axis=1)
+
+
+newBatData = newBatData.add_prefix('b-')
+newBatData = newBatData.rename(columns={'b-Pos': 'Pos', 'b-Name': 'Name'})
+
 #Add uniform numbers from uniformData to end of rosterData
-merge_rost_uni = rosterData.merge(uniformData, how='inner', on='Name')
+merge_rost_uni = newRosterData.merge(uniformData, how='inner', on='Name')
 
 #Rename new rosterData columns
 merge_rost_uni = merge_rost_uni.rename(columns={'#': 'Uniform#'})
@@ -28,7 +34,7 @@ merge_rost_uni = merge_rost_uni.rename(columns={'#': 'Uniform#'})
 #print(uniformData)
 #print(rosterData)
 #pd.options.display.max_columns = None
-print(merge_rost_uni.keys())
-test = merge_rost_uni[['Name', 'Age', 'B', 'T']]
-print(merge_rost_uni[:4])
+print(newBatData.keys())
+# test = merge_rost_uni[['Name', 'Age', 'B', 'T']]
+print(newBatData[:4])
 
